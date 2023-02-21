4ddf9a54dca35193a340af1216dada5065bc4bd7
Kyle Harrington <czi@kyleharrington.com>
2023-02-21 14:52:48 -0500

    Move napari import to only be used in standalone

--- src/napari_qrcode/_widget.py
+++ src/napari_qrcode/_widget.py
@@ -1,8 +1,6 @@
 from magicgui import magic_factory
 import qrcode
 
-import napari
-
 import numpy as np
 
 
@@ -12,11 +10,10 @@ import numpy as np
 def qrcode_widget(
     viewer: "napari.viewer.Viewer",
     text: str = "Check out napari: https://napari.org",
-    box_size = 3,
-    border = 4,
+    box_size=3,
+    border=4,
 ):
-    
-    qr = qrcode.QRCode(box_size = box_size, border = border)
+    qr = qrcode.QRCode(box_size=box_size, border=border)
     qr.add_data(text)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
@@ -25,11 +22,12 @@ def qrcode_widget(
 
 
 if __name__ == "__main__":
+    import napari
+
     viewer = napari.Viewer()
     viewer.window.resize(800, 600)
 
     widget = qrcode_widget()
-    
+
     viewer.window.add_dock_widget(widget, name="QR-Code")
     # widget_demo.show()
-
