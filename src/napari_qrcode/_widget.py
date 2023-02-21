5ee1b364f9e1a2f5ae00eb21547efa2b8a115ec5
Kyle Harrington <czi@kyleharrington.com>
2023-02-21 14:49:37 -0500

    Should be working

--- src/napari_qrcode/_widget.py
+++ src/napari_qrcode/_widget.py
@@ -1,46 +1,35 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
-
-Replace code below according to your needs.
-"""
-from typing import TYPE_CHECKING
-
 from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+import qrcode
 
-if TYPE_CHECKING:
-    import napari
+import napari
 
-
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+import numpy as np
 
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+@magic_factory(
+    call_button="Generate",
+)
+def qrcode_widget(
+    viewer: "napari.viewer.Viewer",
+    text: str = "Check out napari: https://napari.org",
+    box_size = 3,
+    border = 4,
+):
+    
+    qr = qrcode.QRCode(box_size = box_size, border = border)
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    viewer.add_image(np.asarray(img), name=text)
 
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+if __name__ == "__main__":
+    viewer = napari.Viewer()
+    viewer.window.resize(800, 600)
+
+    widget = qrcode_widget()
+    
+    viewer.window.add_dock_widget(widget, name="Conference")
+    # widget_demo.show()
+
